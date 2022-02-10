11cccf4877a2694c555de8674544eaba193c8791
Stefan <stefangorgevik@hotmail.com>
2022-02-10 09:00:11 +0100

    added store

--- meal-app/App.js
+++ meal-app/App.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { useScreens } from 'react-native-screens';
-
-import MealsNavigator from './navigation/MealsNavigator';
-
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { useScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
+import { Provider } from "react-redux";
 useScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
 
@@ -27,5 +33,9 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />;
+    </Provider>
+  );
 }
